5b2938db0ad99219086b7eea8fcf91dc3ddf4209
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-07 18:38:40 +0200

    Header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,13 @@
 <body>
     <header>
-        <p>ciaone</p>
+        <div class="container container--l">
+            <img src="{{ asset('images/marchio-sito-test.png') }}" alt="la-molisana-logo">
+            <nav class="main__nav">
+                <ul>
+                    <li><a href="#" class="active">Home</a></li>
+                    <li><a href="#">Prodotti</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+            </nav>
+        </div>
     </header>
