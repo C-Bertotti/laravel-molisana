a206b60ed696737fa3f30469f5c48bf4ce343ef6
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-10 16:55:58 +0200

    Collegata pagina prodotto

---
+++ resources/views/prodotto.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.mainLay')
+
+@section('pageTitle')
+    La Molisana - I prodotti
+@endsection
+
+@section('main__content')
+    <div class="container container--l">
+        prova
+    </div>
+@endsection
