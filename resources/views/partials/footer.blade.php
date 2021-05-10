a026ace2293e62f9e0997dd9cad42ad28c02aaf1
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-10 20:12:45 +0200

    Footer: scheletro UI

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,8 +1,20 @@
     <footer>
         <div class="container container--l">
-            La Molisana
+            <div class="footer__top left">
+                <img src="{{ asset('images/marchio-sito-test.png') }}" alt="la-molisana-logo">
+                
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsa libero similique repellat perferendis ratione iste odio dolorum eum animi. Soluta maiores perspiciatis assumenda eligendi rerum, perferendis enim quisquam ipsum.</p>
+            </div>
+            <div class="footer__top center">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsa libero similique repellat perferendis ratione iste odio dolorum eum animi. Soluta maiores perspiciatis assumenda eligendi rerum, perferendis enim quisquam ipsum.</p>
+            </div>
+            <div class="footer__top right">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsa libero similique repellat perferendis ratione iste odio dolorum eum animi. Soluta maiores perspiciatis assumenda eligendi rerum, perferendis enim quisquam ipsum.</p>
+            </div>
 
+            
         </div>
+        <img class="footer__bottom" src="{{ asset('images/footer-montagne-bottom.jpg') }}" alt="la-molisana-logo">
     </footer>
 </body>
 </html>
