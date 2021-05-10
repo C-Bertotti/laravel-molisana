3a980cd525181e395cd1bc4478e554605f0a2264
Caterina Bertotti <bertotti4997@gmail.com>
2021-05-10 19:55:36 +0200

    Classe active alla pagina attiva

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,9 +4,9 @@
             <img src="{{ asset('images/marchio-sito-test.png') }}" alt="la-molisana-logo">
             <nav class="main__nav">
                 <ul>
-                    <li><a href="{{route('homepage')}}" class="active">Home</a></li>
-                    <li><a href="{{route('prodotto', ['id' => 0])}}">Prodotti</a></li>
-                    <li><a href="{{route('news')}}">News</a></li>
+                    <li><a href="{{route('homepage')}}" class="{{Route::getCurrentRoute()->getName() == 'homepage' ? 'active' : '' }}">Home</a></li>
+                    <li><a href="{{route('prodotto', ['id' => 0])}}" class="{{Route::getCurrentRoute()->getName() == 'prodotto' ? 'active' : '' }}">Prodotti</a></li>
+                    <li><a href="{{route('news')}}" class="{{Route::getCurrentRoute()->getName() == 'news' ? 'active' : '' }}">News</a></li>
                 </ul>
             </nav>
         </div>
